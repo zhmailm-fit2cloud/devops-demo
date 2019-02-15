f26e4ce174329570fccc1851023d957c7d47ac6c
zhmailm-fit2cloud <huimiao.zhao@fit2cloud.com>
2019-02-15 17:11:06 +0800

    docker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn clean package -Dmaven.test.skip=true
 
-docker build -t registry.fit2cloud.com/fit2cloud2/devops:${branch} .
-docker push registry.fit2cloud.com/fit2cloud2/devops:${branch}
+docker build -t registry.fit2cloud.com/north/miao/devops:${branch} .
+docker push registry.fit2cloud.com//north/miao/devops:${branch}
 
