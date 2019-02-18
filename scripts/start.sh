0edfd5e65e449eb13bae4402081b6dfda0faea02
zhmailm-fit2cloud <huimiao.zhao@fit2cloud.com>
2019-02-18 12:56:35 +0800

    测试

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,6 @@ cd /opt/fit2cloud-demo
 echo going to start pull
 docker pull registry.fit2cloud.com/north/miao/devops:#BUILD_NUMBER#
 echo going to start run
-docker run -it -p 8081:8081 registry.fit2cloud.com/north/miao/devops:#BUILD_NUMBER#
+docker run -it -d -p 8081:8081 registry.fit2cloud.com/north/miao/devops:#BUILD_NUMBER#
 docker ps | grep registry.fit2cloud.com/north/miao/devops | grep -v grep
 echo app started!
