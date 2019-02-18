217988d1c70f0da2d682cb5e6927834d45ac4853
zhmailm-fit2cloud <huimiao.zhao@fit2cloud.com>
2019-02-18 11:29:28 +0800

    测试

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 cd /opt/fit2cloud-demo
 echo going to start app
-docker pull registry.fit2cloud.com/north/miao/devops:#BUILD_NUMBER#
-docker run -it registry.fit2cloud.com/north/miao/devops:#BUILD_NUMBER#
+docker pull registry.fit2cloud.com/north/miao/devops:test
+docker run -it registry.fit2cloud.com/north/miao/devops:test
 echo app started!
-ps aux | grep SimpleHTTPServer | grep -v grep
+ps aux | grep /opt/fit2cloud-demo/tao-demo-app-1.0.jar | grep -v grep
