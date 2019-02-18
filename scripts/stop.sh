8e58c072ea292f36892b4080576aaf4fdd1f2146
zhmailm-fit2cloud <huimiao.zhao@fit2cloud.com>
2019-02-18 14:30:43 +0800

    测试

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,8 +5,7 @@ echo process number is $processesNum
 if [ -n "$processesNum" ];then
     docker stop $processesNum
 fi
-validateNum=`docker ps | grep registry.fit2cloud.com/north/miao/devops | grep -v grep | wc -l | sed 's/ //
-g'`
+validateNum=`docker ps | grep registry.fit2cloud.com/north/miao/devops | grep -v grep | wc -l | sed 's/ //g'`
 echo process validateNum is $validateNum
 if [ "$validateNum" == "0" ];then
     echo stopped app successfully!
