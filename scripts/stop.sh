bba536a51649f70c010dd1c7a1200ca739c26a34
zhmailm-fit2cloud <huimiao.zhao@fit2cloud.com>
2019-02-18 14:19:09 +0800

    测试

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo going to stop app
-docker stop $(docker ps | grep registry.fit2cloud.com/north/miao/devops | awk '{ print $1}')
-processesNum=`docker ps | grep registry.fit2cloud.com/north/miao/devops | grep -v grep | wc -l | sed 's/ //g'`
-if [ "$processesNum" == "0" ];then
+processesNum=`docker ps | grep registry.fit2cloud.com/north/miao/devops | awk '{ print $1}'`
+echo process number is $processesNum
+if [ -n "$processesNum" ];then
+    docker stop $processesNum
+fi
+validateNum=`docker ps | grep registry.fit2cloud.com/north/miao/devops | grep -v grep | wc -l | sed 's/ //
+g'`
+echo process validateNum is $validateNum
+if [ "$validateNum" == "0" ];then
     echo stopped app successfully!
     exit 0
 else
     echo stopped app failed!
     exit 1
-fi 
+fi
