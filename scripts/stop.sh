455967bcc803a37bd2f048d7d7b3c29e4b55937f
zhmailm-fit2cloud <huimiao.zhao@fit2cloud.com>
2019-02-18 11:36:15 +0800

    测试

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo going to stop app
-ps aux | grep /opt/fit2cloud-demo/tao-demo-app-1.0.jar | grep 8081 | awk '{print "kill -9 " $2}' | bash
-processesNum=`ps aux | grep /opt/fit2cloud-demo/tao-demo-app-1.0.jar | grep 8081 | grep -v grep | wc -l | sed 's/ //g'`
+docker stop $(docker ps | grep registry.fit2cloud.com/north/miao/devops | awk "{print $1}") | bash
+processesNum=`docker ps | grep registry.fit2cloud.com/north/miao/devops | grep -v grep | wc -l | sed 's/ //g'`
 if [ "$processesNum" == "0" ];then
     echo stopped app successfully!
     exit 0
